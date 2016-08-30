39180a1d9cd13d9ae9dbe2d4bb305c4bba13e498
AyrFX <oleg@gbg.bg>
2016-08-30 10:42:28 +0300

    Added task 2 of Functions and Function Expressions homework

--- 02. Functions-and-Function-Expressions/tasks/task-2.js
+++ 02. Functions-and-Function-Expressions/tasks/task-2.js
@@ -7,7 +7,52 @@
 */
 
 function findPrimes() {
-	
+	var allPrimes = [],
+		primesInRange = [],
+		i;
+
+	if (arguments.length !== 2) {
+		throw 'Given parameters are less or more than two!';
+	}
+
+	if (!isNumeric(arguments[0]) || !isNumeric(arguments[1])) {
+		throw 'All parameters must be convertable to number!';
+	}
+
+	allPrimes = findPrimes(arguments[1]);
+	for (i = 0; i < allPrimes.length; i += 1) {
+		if (arguments[0] <= allPrimes[i] && allPrimes[i] <= arguments[1]) {
+			primesInRange.push(allPrimes[i]);
+		}
+	}
+
+	return primesInRange;
+
+	function isNumeric(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+
+	function findPrimes(rangeTop) {
+		var i,
+			j,
+			primes = [],
+			isPrime;
+
+		for (i = 2; i <= rangeTop; i += 1) {
+			isPrime = true;
+			for (j = 2; j < i; j += 1) {
+				isPrime = true;
+				if (i % j === 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime) {
+				primes.push(i);
+			}
+		}
+		return primes;
+	}
 }
 
 module.exports = findPrimes;
