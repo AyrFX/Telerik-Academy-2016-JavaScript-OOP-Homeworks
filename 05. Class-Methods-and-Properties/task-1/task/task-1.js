4b89bc970d5a0842774a64f81b5d98d5c24ab856
AyrFX <oleg@gbg.bg>
2016-09-01 18:03:42 +0300

    Added Class Methods and Properties Homework (2 tests passing)

--- 05. Class-Methods-and-Properties/task-1/task/task-1.js
+++ 05. Class-Methods-and-Properties/task-1/task/task-1.js
@@ -2,13 +2,118 @@
 
 class listNode {
     constructor(value) {
-        
+        this._data = value;
+        this._next = null;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get next() {
+        return this._next;
+    }
+
+    set next(value) {
+        this._next = value;
     }
 }
 
+
+
 class LinkedList {
     constructor() {
+        this._length = 0;
+        this._head = null;
 
+        return this;
+    }
+
+    //properties
+
+    get first() {
+        if (this._head !== null) {
+            return this._head.data;
+        }
+        return undefined;
+    }
+
+    get last() {
+        var currentNode;
+
+        if (this._head !== null) {
+            currentNode = this._head;
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
+            }
+            return currentNode.data;
+        }
+        return undefined;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    get lastNode() {
+        var currentNode;
+
+        if (this._head !== null) {
+            currentNode = this._head;
+            while (currentNode.next !== null) {
+                currentNode = currentNode.next;
+            }
+            return currentNode;
+        }
+        return undefined;
+    }
+
+    //methods
+    append(...nodes) {
+        var lastNode = this.lastNode,
+            newNode,
+            i;
+
+        if (this._length === 0) {
+            this._head = new listNode(nodes[0]);
+            lastNode = this._head;
+            this._length += 1;
+            for (i = 1; i < nodes.length; i += 1) {
+                newNode = new listNode(nodes[i]);
+                lastNode.next = newNode;
+                lastNode = newNode;
+                this._length += 1;
+            }
+        } else {
+            for (i = 0; i < nodes.length; i += 1) {
+                newNode = new listNode(nodes[i]);
+                lastNode.next = newNode;
+                lastNode = newNode;
+                this._length += 1;
+            }
+        }
+
+        return this;
+    }
+
+    toString() {
+        var i,
+            currentNode = this._head,
+            result = '';
+
+        if (currentNode === null) {
+            return '';
+        }
+        while (currentNode !== null) {
+            if (currentNode === this._head) {
+                result += `${currentNode.data}`;
+            } else {
+                result += ` -> ${currentNode.data}`;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return result;
     }
 }
 
--- 05. Class-Methods-and-Properties/task-1/tests/tests.js
+++ 05. Class-Methods-and-Properties/task-1/tests/tests.js
@@ -9,7 +9,7 @@ describe('Linked list: ', () => {
 
         const list = new LinkedList(),
             values = [1, 2, false, 3, 4];
-        
+
         list.append(...values);
 
         expect(list.first).to.equal(values[0]);
@@ -21,10 +21,10 @@ describe('Linked list: ', () => {
     it('append should implement chaining and toString should work correctly', () => {
         const values = [1, 2, 3, 4, 5, 6],
             list = new LinkedList()
-                            .append(1, 2)
-                            .append(3, 4)
-                            .append(5)
-                            .append(6);
+            .append(1, 2)
+            .append(3, 4)
+            .append(5)
+            .append(6);
 
 
         expect(list.first).to.equal(values[0]);
@@ -37,11 +37,11 @@ describe('Linked list: ', () => {
 
         const values = [0, 1, 2, 3, 4, 5],
             list = new LinkedList()
-                            .append(3, 4)
-                            .prepend(1, 2)
-                            .prepend(0)
-                            .append(5);
-        
+            .append(3, 4)
+            .prepend(1, 2)
+            .prepend(0)
+            .append(5);
+
 
         expect(list.first).to.equal(values[0]);
         expect(list.last).to.equal(values[values.length - 1]);
@@ -52,7 +52,7 @@ describe('Linked list: ', () => {
     it('should insert correctly', () => {
         const values = [1, 2, 6, 7, 8],
             list = new LinkedList().append(...values).insert(2, 3, 4).insert(4, 5);
-        
+
         expect(list.first).to.equal(1);
         expect(list.length).to.equal(8);
         expect(list.toString()).to.equal([1, 2, 3, 4, 5, 6, 7, 8].join(' -> '));
@@ -62,7 +62,7 @@ describe('Linked list: ', () => {
         const list = new LinkedList().append(1, 2).insert(0, 3, 4);
 
         list.insert(list.length - 1, 'kremikovci');
-        
+
         expect(list.first).to.equal(3);
         expect(list.last).to.equal('kremikovci');
         expect(list.length).to.equal(5);
@@ -74,31 +74,36 @@ describe('Linked list: ', () => {
         const values = [5, 6, 38],
             list = new LinkedList().append(...values);
 
-        for(const val of list) {
+        for (const val of list) {
             expect(values.indexOf(val)).to.not.equal(-1);
         }
     });
 
     it('should have correct for-of', () => {
 
-        const values = [5, 6, 3, 'gosho', true, null, 'ivan', { message: 'Hello' }],
+        const values = [5, 6, 3, 'gosho', true, null, 'ivan', {
+                message: 'Hello'
+            }],
             list = new LinkedList()
-                            .append(...values.slice(4))
-                            .prepend(...values.slice(0, 4));
+            .append(...values.slice(4))
+            .prepend(...values.slice(0, 4));
 
-        for(const val of list) {
+        for (const val of list) {
             expect(val).to.equal(values.shift());
         }
     });
 
     it('should have correct removeAt', () => {
-        const values = ['test', true, null, 1, 2, 'testtest', { value: 'val', message: 'hello' }, 'gg'],
+        const values = ['test', true, null, 1, 2, 'testtest', {
+                value: 'val',
+                message: 'hello'
+            }, 'gg'],
             list = new LinkedList().append(...values),
             removed1 = list.removeAt(1),
             removed2 = list.removeAt(1),
             removed3 = list.removeAt(0),
             removed4 = list.removeAt(list.length - 1);
-        
+
         expect(list.first).to.equal(1);
         expect(list.last).to.equal('gg');
         expect(list.length).to.equal(values.length - 4);
@@ -110,7 +115,7 @@ describe('Linked list: ', () => {
             list = new LinkedList().append(...values),
             listLength = list.length;
 
-        for(let i = 0, length = values.length; i < length; i += 1) {
+        for (let i = 0, length = values.length; i < length; i += 1) {
             expect(list.at(i)).to.equal(values[i]);
         }
 
@@ -136,9 +141,9 @@ describe('Linked list: ', () => {
             list = new LinkedList().append(...values),
             listLength = list.length;
 
-        for(let i = 0, length = values.length; i < length; i += 1) {
-           list.at(i, i);
-           expect(list.at(i)).to.equal(i);
+        for (let i = 0, length = values.length; i < length; i += 1) {
+            list.at(i, i);
+            expect(list.at(i)).to.equal(i);
         }
 
         expect(list.first).to.equal(0);
@@ -148,7 +153,10 @@ describe('Linked list: ', () => {
 
     it('should have correct toArray', () => {
 
-        const values = ['test', true, null, 1, 2, 'testtest', { value: 'val', message: 'hello' }],
+        const values = ['test', true, null, 1, 2, 'testtest', {
+                value: 'val',
+                message: 'hello'
+            }],
             array = new LinkedList().append(...values).toArray();
 
         expect(array instanceof Array).to.be.true;
